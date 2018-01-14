ecad557251bd1e1538178dc6c5d9b55c866102d0
Sam Meech-Ward <sam@meech-ward.com>
2018-01-14 06:42:05 -0800

    Remove cocoapods and carthage

--- CocoaPodsExample/Pods/Target Support Files/AudioRecordingVisualizer/AudioRecordingVisualizer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AudioRecordingVisualizer : NSObject
-@end
-@implementation PodsDummy_AudioRecordingVisualizer
-@end
--- CocoaPodsExample/Pods/Target Support Files/Pods-AudioRecordingVisualizer_Example/Pods-AudioRecordingVisualizer_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AudioRecordingVisualizer_Example : NSObject
-@end
-@implementation PodsDummy_Pods_AudioRecordingVisualizer_Example
-@end
